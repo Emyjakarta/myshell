b9039f09491b8570de7b0296599c57dfc46154a2
Emyjakarta <ukonud4@gmail.com>
2023-11-21 18:53:25 +0100

    Debug

--- execute_command.c
+++ execute_command.c
@@ -10,7 +10,7 @@ void execute_command(char *cmd, char **args)
 	int status = 0;
 	pid_t pid;
 
-	if (access(cmd, F_OK) == 0)
+	if (access(cmd, X_OK) == 0)
 	{
 		pid = fork();
 		if (pid < 0)
@@ -20,37 +20,41 @@ void execute_command(char *cmd, char **args)
 		}
 		else if (pid == 0)
 		{
-			if (execve(cmd, args, environ) == -1)
+			if (cmd[0] != '/')
 			{
-				perror("execve");
-				exit(EXIT_FAILURE);
+				path = getenv("PATH");
+				copy_path = strdup(path);
+				dir = strtok(copy_path, ":");
+				while(dir != NULL)
+				{
+					dir_len = strlen(dir);
+					arg_len = strlen(args[0]);
+					if (dir_len + 1 + arg_len >= PATH_MAX)
+					{
+						fprintf(stderr, "Error: PATH length exceeds maximum length\n");
+						break;
+					}
+					strcpy(command_path, dir);
+					dir_len = strlen(dir);
+					command_path[dir_len] = '/';
+					strcpy(command_path + dir_len + 1, args[0]);
+					command_path[dir_len + 1 + arg_len] = '\0';
+					if (access(command_path, F_OK) == 0)
+					{
+						if (execve(command_path, args, environ) == -1)
+							perror("execve");
+						break;
+					}
+					dir = strtok(NULL, ":");
+				}
 			}
-		}
-		else if (pid == 0 && cmd[0] != '/')
-		{
-			path = getenv("PATH");
-			copy_path = strdup(path);
-			dir = strtok(copy_path, ":");
-			while(dir != NULL)
+			else
 			{
-				dir_len = strlen(dir);
-				arg_len = strlen(args[0]);
-				if (dir_len + 1 + arg_len >= PATH_MAX)
+				if (execve(cmd, args, environ) == -1)
 				{
-					fprintf(stderr, "Error: PATH length exceeds maximum length\n");
-					break;
+					perror("execve");
+					exit(EXIT_FAILURE);
 				}
-				strcpy(command_path, dir);
-				command_path[dir_len] = '/';
-				strcpy(command_path + dir_len + 1, args[0]);
-				command_path[dir_len + 1 + arg_len] = '\0';
-				if (access(command_path, F_OK) == 0)
-				{
-					if (execve(command_path, args, environ) == -1)
-						perror("execve");
-					break;
-				}
-				dir = strtok(NULL, ":");
 			}
 			fprintf(stderr, "%s: command not found\n", args[0]);
 			exit(EXIT_FAILURE);
