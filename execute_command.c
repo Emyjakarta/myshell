af5d121b18a57f37face5b65ebc8382c42ffb50c
Emyjakarta <ukonud4@gmail.com>
2023-11-21 17:57:41 +0100

    still debugging

--- execute_command.c
+++ execute_command.c
@@ -6,7 +6,7 @@ void execute_command(char *cmd, char **args)
 	char *dir = NULL, *copy_path = NULL;
 	size_t dir_len, arg_len;
 	/*char *delim = " \t", *token = NULL;*/
-/*	char *com[MAX_COMMAND_ARGS] = strdup(*args);*/
+	/*	char *com[MAX_COMMAND_ARGS] = strdup(*args);*/
 	int status = 0;
 	pid_t pid;
 
@@ -26,38 +26,38 @@ void execute_command(char *cmd, char **args)
 				exit(EXIT_FAILURE);
 			}
 		}
+		else if (pid == 0 && cmd[0] != '/')
+		{
+			path = getenv("PATH");
+			copy_path = strdup(path);
+			dir = strtok(copy_path, ":");
+			while(dir != NULL)
+			{
+				dir_len = strlen(dir);
+				arg_len = strlen(args[0]);
+				if (dir_len + 1 + arg_len >= PATH_MAX)
+				{
+					fprintf(stderr, "Error: PATH length exceeds maximum length\n");
+					break;
+				}
+				strcpy(command_path, dir);
+				command_path[dir_len] = '/';
+				strcpy(command_path + dir_len + 1, args[0]);
+				command_path[dir_len + 1 + arg_len] = '\0';
+				if (access(command_path, F_OK) == 0)
+				{
+					if (execve(command_path, args, environ) == -1)
+						perror("execve");
+					break;
+				}
+				dir = strtok(NULL, ":");
+			}
+			fprintf(stderr, "%s: command not found\n", args[0]);
+			exit(EXIT_FAILURE);
+		}
 		else
 		{
 			waitpid(pid, &status, 0);
 		}
-	}
-	else
-	{
-		path = getenv("PATH");
-		copy_path = strdup(path);
-		dir = strtok(copy_path, ":");
-		while(dir != NULL)
-		{
-			dir_len = strlen(dir);
-			arg_len = strlen(args[0]);
-			if (dir_len + 1 + arg_len >= PATH_MAX)
-			{
-				fprintf(stderr, "Error: PATH length exceeds maximum length\n");
-				break;
-			}
-			strcpy(command_path, dir);
-			command_path[dir_len] = '/';
-			strcpy(command_path + dir_len + 1, args[0]);
-			command_path[dir_len + 1 + arg_len] = '\0';
-			if (access(command_path, F_OK) == 0)
-			{
-				if (execve(command_path, args, environ) == -1)
-					perror("execve");
-				break;
-			}
-			dir = strtok(NULL, ":");
-		}
-		fprintf(stderr, "%s: command not found\n", args[0]);
-		exit(EXIT_FAILURE);
 	}
 }
