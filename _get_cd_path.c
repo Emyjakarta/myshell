66cc9ca604d6e42b91a01a6472a23d4181e2a993
Emyjakarta <ukonud4@gmail.com>
2024-01-07 18:01:33 +0100

    clean

--- _get_cd_path.c
+++ _get_cd_path.c
@@ -7,22 +7,14 @@
  */
 char *_get_cd_path(const char *_command, char **arguments)
 {
-	static char cd_path[MAX_PATH_LENGTH]; /*Pre-allocated buffer for storing cd path*/
+	/*Pre-allocated buffer for storing cd path*/
+	static char cd_path[MAX_PATH_LENGTH];
 	char *_oldpwd = NULL;
 
-	printf("Entered _get_cd_path function\n");
-
 	if (_command == NULL || strcmp(_command, "cd") != 0 || arguments == NULL)
-	{
-		printf("Error: Invalid input for _get_cd_path\n");
 		return (NULL);
-	}
-
 	if (arguments[1] == NULL)
-	{
-		printf("No arguments provided, returning HOME directory\n");
 		return (getenv("HOME"));
-	}
 	printf("arguments[1]: %s\n", arguments[1]);
 	_oldpwd = getenv("OLDPWD");
 	printf("OLDPWD: %s\n", _oldpwd);
@@ -37,7 +29,6 @@ char *_get_cd_path(const char *_command, char **arguments)
 	if (access(arguments[1], F_OK) == 0 &&
 			access(arguments[1], R_OK | X_OK) == 0)
 	{
-		printf("Valid directory: %s\n", arguments[1]);
 		if (strlen(arguments[1]) < MAX_PATH_LENGTH)
 		{
 			strcpy(cd_path, arguments[1]);
